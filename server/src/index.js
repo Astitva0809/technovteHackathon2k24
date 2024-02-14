8b1a4f1671f6a1b3a579a4b9d9871a10ef6a6aa5
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-02-14 19:03:03 +0530

    project setup

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import connectDB from "../db/index.js";
+import dotenv from "dotenv";
+import { app, port } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log(`Server is running at: http://localhost:${port}`)
+    })
+})
+.catch((error)=>{
+    console.log(error);
+})
