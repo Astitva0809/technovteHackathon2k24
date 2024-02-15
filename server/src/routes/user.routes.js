df1a00b51600c2c7266e8568505cc7ffeab9a0e7
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-02-15 17:27:36 +0530

    added logout

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registerUser, logInUser } from "../controllers/user.controller.js";
-
+import { registerUser, logInUser, logOutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 //Register
 router.route("/register").post(registerUser)
 router.route("/login").post(logInUser)
+router.route("/logout").post(verifyJWT, logOutUser)
 export default router;
