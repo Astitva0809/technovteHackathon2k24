65ec0ac649b0c3de0fe79e1068f758ea77ee0b94
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-02-15 16:40:37 +0530

    added login logic

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -4,6 +4,26 @@ import {ApiResponse} from '../utils/ApiResponse.js';
 import {User} from '../models/user.model.js';
 import emailValidator from 'email-validator';
 
+const generateAccessAndRefreshToken = async(userId)=>{
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+
+        return {accessToken, refreshToken};
+
+    } catch (error) {
+        return res
+        .status(500)
+        .json(
+            new ApiError(500, "Something went wrong while generating refresh and access token!!")
+        );
+    }
+}
+
 const registerUser = asyncHandler(async (req,res)=>{
  
     const {email, userName, password} = req.body;
@@ -67,4 +87,78 @@ const registerUser = asyncHandler(async (req,res)=>{
     );
 })
 
-export {registerUser};
+const logInUser = asyncHandler(async(req,res)=>{
+
+    const {email, password} = req.body;
+
+    if(!email){ 
+        return res
+        .status(400)
+        .json(
+            new ApiError(400,"Email is required!!")
+        )
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        return res
+        .status(400)
+        .json(
+            new ApiError(
+                400,
+                "User not found"
+            )
+        )
+    }
+
+    if(!password){
+        return res
+        .status(401)
+        .json(
+            new ApiError(
+                401,
+                "Password is Required!!"
+            )
+        )
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        return res
+        .status(401)
+        .json(
+            new ApiError(
+                401,
+                "Invalid Password!!"
+            )
+        )
+    }
+
+
+   const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+
+   const loggedInUser = await User.findById(user._id) .select("-password -refreshToken");
+
+   const options = {
+     httpOnly: true,
+     secure: true
+   };
+
+   return res.status(200)
+   .cookie('accessToken', accessToken, options)
+   .cookie('refreshToken', refreshToken, options)
+   .json(
+    new ApiResponse(
+        200,
+        {
+            user: loggedInUser, accessToken, refreshToken
+        },
+        "User logged In Successfully!!"
+    )
+   )
+
+})
+
+export {registerUser, logInUser};
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, logInUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 //Register
 router.route("/register").post(registerUser)
-
+router.route("/login").post(logInUser)
 export default router;
